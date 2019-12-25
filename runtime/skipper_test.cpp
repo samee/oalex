cc9c1c26b08cbc5e8425fd2aaa4cdeb1d5a8109f
Samee Zahur <szahur@google.com>
2019-12-25 15:00:28 -0800

    Adding automatic getch() conversion for Skipper.
    This was a TODO in skipper_test.cpp. We may move it to the library later
    if this becomes useful. For now, it only serves the tests.
    
    I'm also slightly surprised that I didn't catch the 7-->8 change
    earlier. Did I not run unit tests when I added Haskell-style comments?
    That's odd.

--- runtime/skipper_test.cpp
+++ runtime/skipper_test.cpp
@@ -13,12 +13,18 @@
     limitations under the License. */
 
 #include "skipper.h"
+#include <functional>
+#include <string_view>
 #include <vector>
 #include "diags.h"
 #include "diags_test_util.h"
+#include "test_util.h"
+using std::function;
 using std::vector;
 using std::string;
+using std::string_view;
 using oalex::Bug;
+using oalex::GetFromString;
 using oalex::Input;
 using oalex::InputDiags;
 using oalex::Skipper;
@@ -26,22 +32,77 @@ using oalex::operator<<;
 
 namespace {
 
-// TODO avoid explicit '\n' in inputs with a custom getch().
+// Takes in an Input::getch_() callback, and produces another that conforms
+// to the needs of Skipper. Does a poor-man's conversion from dos-encoded crlf
+// streams to Unix-style lf streams. Additionally, it adds an extra '\n'
+// character at the end if one doesn't already exist.
+//
+// Normally, the standard file I/O libraries will take care of this, so we
+// shouldn't need this Unixify adaptor. But Skipper really cares about the last
+// character being a '\n' for single-line comments.
+//
+// We try to do this in a way that's idempotent: it won't modify anything if
+// the stream already has everything we need. It won't add an extra '\n' in the
+// end if one is already there. The hope is there won't be a need to ever
+// switch this translation off. If that becomes necessary, we'll probably
+// do the simpler thing of just using <stdio.h> streams and adding an extra
+// '\n' to everything, instead of using this.
+//
+// Usage:
+//   std::function<int16_t()> newGetch = Unixify{oldGetch};
+struct Unixify {
+  function<int16_t()> getch;
+  int16_t last_char = -1;
+  int16_t operator()() {
+    int16_t ch;
+    do { ch = getch(); } while(ch == '\r');  // filter out '\r'.
+
+    if(ch < 0 && last_char >= 0 && last_char != '\n') return last_char='\n';
+    else return last_char=ch;
+  }
+};
+
+string escape(string_view s) {
+  string rv;
+  for(char ch : s) switch(ch) {
+    case '\r': rv += "\\r"; break;
+    case '\n': rv += "\\n"; break;
+    default: rv += ch;
+  }
+  return rv;
+}
+
+void testUnixify(const char input[], const char expected_output[]) {
+  auto getch = Unixify{GetFromString(input)};
+  string output;
+  while(true) { char ch = getch(); if(ch < 0) break; output += ch; }
+  if(expected_output != output)
+    BugMe<<escape(expected_output)<<" != "<<escape(output);
+}
+
+void unixifyTests() {
+  testUnixify("foo\r\nbar", "foo\nbar\n");
+  testUnixify("foo\r\nbar\r\nbaz", "foo\nbar\nbaz\n");
+  testUnixify("foo\nbar\n", "foo\nbar\n");
+  testUnixify("", "");
+  testUnixify("foo", "foo\n");
+}
+
 // TODO failure tests.
 const Skipper cskip{ {{"/*","*/"},{"//","\n"}}, {}};
-const char cinput[] = "hello /* comment */ world // more stuff\n";
+const char cinput[] = "hello /* comment */ world // more stuff";
 
 const Skipper pyskip{ {{"#","\n"},{"'''","'''"},{"\"\"\"","\"\"\""}}, {}};
-const char pyinput[] = "hello '''comment'''\"\"\"comment\"\"\" world # stuff\n";
+const char pyinput[] = "hello '''comment'''\"\"\"comment\"\"\" world # stuff";
 
 const Skipper ocamlskip{{}, {{"(*","*)"}} };
-const char ocamlinput[] = "hello (* more (* nested *) stuff *) world\n";
+const char ocamlinput[] = "hello (* more (* nested *) stuff *) world";
 
 const Skipper htmlskip{{{"<!--","-->"}}, {}};
-const char htmlinput[] = "hello <!-- comment --> world\n";
+const char htmlinput[] = "hello <!-- comment --> world";
 
 const Skipper haskellskip{{{"--","\n"}}, {{"{-","-}"}}};
-const char haskellinput[] = "hello {- a {- b -} c -} world -- stuff\n";
+const char haskellinput[] = "hello {- a {- b -} c -} world -- stuff";
 
 vector<string> getWords(InputDiags& ctx, const Skipper& skip) {
   const Input& input = ctx.input;
@@ -63,10 +124,10 @@ vector<string> getWords(InputDiags& ctx, const Skipper& skip) {
 void testSingleLineSuccess() {
   const Skipper *skip[] = {&cskip,&pyskip,&ocamlskip,&htmlskip,&haskellskip};
   const char *input[] = {cinput,pyinput,ocamlinput,htmlinput,haskellinput};
-  const char langnames[][7] = {"c","python","ocaml","html","haskell"};
+  const char langnames[][8] = {"c","python","ocaml","html","haskell"};
   const size_t n = sizeof(skip)/sizeof(skip[0]);
   for(size_t i=0; i<n; ++i) {
-    InputDiags ctx = testInputDiags(input[i]);
+    InputDiags ctx{Input(Unixify{GetFromString(input[i])}), {}};
     vector<string> words = getWords(ctx,*skip[i]);
     if(words != vector<string>{"hello", "world"})
       BugMe<<"Had problems with parsing "<<langnames[i]<<". "<<words
@@ -77,5 +138,6 @@ void testSingleLineSuccess() {
 }  // namespace
 
 int main() {
+  unixifyTests();
   testSingleLineSuccess();
 }
