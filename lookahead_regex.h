8b0516c0c3dd5ae42ddf5aec620f187cabbdca38
Samee Zahur <szahur@google.com>
2020-02-02 15:56:39 -0800

    Starting out regex for lookahead
    It would have been so much easier if I could just reuse an existing
    library. But of course they are not meant to be customizable.

---
+++ lookahead_regex.h
@@ -0,0 +1,67 @@
+/*  Copyright 2020 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License. */
+
+/*
+We have two separate uses for regex, and I'm not entirely sure they match up.
+One is in lookaheads, the other is in the actual grammar. It is possible that
+we'll allow grammars with more flexibility if they are regular (no lookaheads,
+allow negation, use in skipper, so on).
+
+For now though, it looks like we are blocked more on the lookahead part of
+things. So let's design something for just that, keeping in mind that we may
+have to capture subexpressions at some point.
+
+So what operations do I care about? Match prefix and null intersection:
+
+  startsWith(string, regex) -> bool
+  prefixesBoth(regex1, regex2) -> optional<string>
+
+This means I will need to support negative lookaheads, at least in the end.
+Well, I will need to support negation, since this whole thing is a lookahead
+anyway. Since we are only matching and not parsing, we don't care about
+ambiguous disjunctions; we don't care about which branch was taken.
+
+We won't care about greedy-vs-non-greedy repetition either, since I don't care
+about how far they match. Only that they do.
+
+Might parameterize primitives, if we ever use it directly on JsonLoc.
+
+data RegexPrimitive = Char | CharRange
+
+data Regex = Primitive RegexPrimitive
+           | Concat [Regex]
+           | Repeat Regex
+           | Optional Regex
+           | OrList [Regex]
+           | Negate Regex
+
+-- Add escaping parameters.
+prettyPrint Primitive (Char ch) = ch
+prettyPrint Primitive (CharRange from to) = from ++ "-" ++ to
+
+prettyPrint Concat [pieces] = concatmap prettyPrint pieces
+
+-- consider eliminating grouping
+prettyPrint Repeat regex = "(" ++ prettyPrint regex ++ ")..."
+
+prettyPrint Optional regex = "[" ++ prettyPrint regex ++ "]"
+
+
+Okay, todo then:
+  parse regex
+  prettyPrint regex
+  startsWith(input_view, regex) -> bool
+  prefixesBoth(regex1, regex2) -> optional<string>
+
+   */
