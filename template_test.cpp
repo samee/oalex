339d84b13d38cfe32f64d223c68d06617e8ec829
Samee Zahur <szahur@google.com>
2020-05-22 16:16:07 -0700

    Reuse ctx among test QuotedStrings
    This commit replaces the quote() macro with a new fquote() pattern.
    
    Previously, quote() would create a temporary InputDiags to parse it and
    extract a new QuotedString object for use in a test. That temporary
    InputDiags would then go out of scope, and be destroyed. This had some
    unintended consequences:
    
      * QuotedString kept a dangling pointer to InputDiags::diags. This
        was not a problem when quote() was first added, since QuotedString
        had no such pointer member. This was later added to facilitate
        error-reporting.
      * On the topic of error-reporting, every quote would be coming from
        "line 1 col 1" in every related error message, since they were all
        plucked from fresh strings passed to quote(), and not from
        substrings of a larger file.
      * Because the related diags vector was no longer available after
        quote() returned, it was also impossible to invoke
        assertEmptyDiags() after tests.
    
    To remedy this, the test now starts with a large string with an
    associated InputDiags ctx object that stays alive throughout the test.
    An associated fquote() lambda is also produced that finds relevant
    substrings from the larger input and constructs QuotedInput. This will
    also allow us to invoke assertEmptyDiags() when we add diagnostics in
    the following commits.
    
    Alternatively, we could have also designed fquote() to accept an integer
    i instead of a substring, so as to find the i-th string literal. But the
    substring option was considered more readable so that the reader didn't
    have to keep referring back to the full text input and count through it.
    It was also considered less error-prone in the face of insertion and
    deletion of quotes in the larger test input.
    
    Yes, this required us to repeat test substrings in two places, but that
    was considered a reasonable tradeoff for readability.

--- template_test.cpp
+++ template_test.cpp
@@ -14,8 +14,10 @@
 
 #include "template.h"
 #include "lexer.h"
+#include "runtime/input_view.h"
 #include "runtime/diags_test_util.h"
 #include "runtime/test_util.h"
+using std::make_unique;
 using std::optional;
 using std::pair;
 using std::string;
@@ -24,6 +26,7 @@ using std::vector;
 using namespace std::literals::string_literals;
 using oalex::Bug;
 using oalex::DelimPair;
+using oalex::Input;
 using oalex::InputDiags;
 using oalex::matchAllParts;
 using oalex::lex::QuotedString;
@@ -39,31 +42,70 @@ void assertHasSubstr(string_view testName, string_view s,
          <<"' instead of '"<<t<<"'";
 }
 
-QuotedString quoteImpl(string_view testName, string s) {
-  size_t i = 0;
-  InputDiags ctx = testInputDiags('"' + s + '"');
-  auto res = lexQuotedString(ctx, i);
-  assertEmptyDiags(testName, ctx.diags);
-  if(!res.has_value() || i != s.size() + 2)
-    Bug()<<testName<<": quote() called with invalid string "<<s;
-  return *res;
+// Maybe move this to runtime/input_view.h if this becomes useful.
+size_t findSubstr(const Input& input, string_view s) {
+  for(size_t i = 0; input.sizeGt(i); ++i) if(input.hasPrefix(i, s)) return i;
+  return string::npos;
 }
 
-#define quote(arg) quoteImpl(__func__, arg)
+QuotedString findQuote(string_view testName, InputDiags& ctx,
+                       string s) {
+  s = '"' + s + '"';
+  size_t i = findSubstr(ctx.input, s);
+  if(i == string::npos)
+    Bug()<<testName<<": '"<<s<<"' not found in test input";
+  size_t j = i;
+  auto res = lexQuotedString(ctx, j);
+  assertEmptyDiags(testName, ctx.diags);
+  if(!res.has_value() || j != i + s.size())
+    Bug()<<testName<<": findQuote() called with invalid string "<<s;
+  return std::move(*res);
+}
+
+/*
+This function returns a pair of (unique_ptr<InputDiags>, fquote), where:
+  * InputDiags: holds fileBody as a string, surrounded by an extra space
+    character on both sides.
+  * fquote: a lambda that uses findQuote to extract a QuotedString from the
+    returned InputDiags.
+
+Input parameters:
+  * testName: used only for diagnostic messages in case something goes wrong.
+  * fileBody: Usually a concatenation of various string literals used in a test.
+
+Implementation note:
+  * We surrounds fileBody with spaces to prevent internal markUsed() calls from
+    invoking forgetBefore(). This is unnecessary if we are using fileBody
+    contents in only a single test, but this allows convenient reuse.
+  * We cannot return InputDiags by value, since fquote keeps a reference to it.
+*/
+auto setupMatchTest(string testName, string fileBody) {
+  auto ctx = make_unique<InputDiags>(testInputDiags(" " + fileBody + " "));
+  auto fquote = [testName, &ctxref = *ctx](string s) {
+    return findQuote(testName, ctxref, std::move(s));
+  };
+  return make_pair(std::move(ctx), fquote);
+}
 
 void testMatchAll() {
-  const QuotedString tmpl = quote("if (cond) { ... } else { ... }");
+  auto [ctx, fquote] = setupMatchTest(__func__, R"(
+    "if (cond) { ... } else { ... }"
+    "cond"
+    "{" "}"
+  )");
+
+  const QuotedString tmpl = fquote("if (cond) { ... } else { ... }");
 
   // Test a single match.
   optional<vector<pair<size_t, size_t>>> out
-    = matchAllParts(quote("cond"), tmpl);
+    = matchAllParts(fquote("cond"), tmpl);
 
   if(!out) BugMe<<"Couldn't find 'cond' in: "<<string(tmpl);
   if(out->size() != 1)
     BugMe<<"Was expecting a single match. Found "<<out->size();
   assertHasSubstr(__func__, tmpl, out->at(0).first, "cond");
 
-  out = matchAllParts(DelimPair{quote("{"), quote("}")}, tmpl);
+  out = matchAllParts(DelimPair{fquote("{"), fquote("}")}, tmpl);
   if(!out) BugMe<<"Couldn't find '{ ... }' in: "<<string(tmpl);
   if(out->size() != 2)
     BugMe<<"Was expecting two matches. Found "<<out->size();
