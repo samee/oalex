d1654d0214d1a42e1e50e9ba6fd1f6d7a3bd1785
Samee Zahur <szahur@google.com>
2020-10-11 19:43:36 -0700

    Skeletal test linking generated source

---
+++ codegen_gen_test.cpp
@@ -0,0 +1,56 @@
+/*  Copyright 2020 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License. */
+
+#include <unistd.h>
+#include "runtime/util.h"
+#include <cstdio>
+#include <memory>
+#include <string>
+using oalex::Bug;
+using oalex::UserError;
+using std::unique_ptr;
+using std::string;
+
+string parseCmdLine(int argc, char* argv[]) {
+  int opt;
+  string rv;
+  while((opt = getopt(argc, argv, "o:")) != -1) {
+    if(opt == 'o') {
+      if(!optarg) Bug("Null argument to -o");
+      rv = optarg;
+    }else if(opt == '?') UserError("Please check the command line");
+    else {
+     if(optarg) Bug("Got unexpected parameter {}", optarg);
+     return "";
+    }
+  }
+  if(rv.empty()) UserError("Missing -o option for output filename");
+  if(optind < argc) UserError("Extra parameter: {}", argv[optind]);
+  if(optind > argc) Bug("getopt() produced too large an optind");
+  return rv;
+}
+
+int main(int argc, char* argv[]) {
+  try {
+    string outfile = parseCmdLine(argc, argv);
+    unique_ptr<FILE, decltype(&fclose)> fp(fopen(outfile.c_str(), "w"), fclose);
+    if(!fp) UserError("Couldn't write to file {}", outfile);
+    fputs("bool goodFunc() { return true; }\n", fp.get());
+    fputs("bool badFunc()  { return false; }\n", fp.get());
+    return 0;
+  }catch(const oalex::UserErrorEx& ex) {
+    fprintf(stderr, "%s: %s\n", argv[0], ex.what());
+    return 1;
+  }
+}
