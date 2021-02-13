59766004986b2fa6878a1a2cc1e438a7e3938860
Samee Zahur <szahur@google.com>
2021-02-12 20:20:20 -0800

    Organize tests into separate functions
    I was starting to have trouble understanding where one test ends and the
    other ends. This should also help me reason about the scope of some
    variables.
    
    No functional change.

--- oalex_main_test.py
+++ oalex_main_test.py
@@ -21,22 +21,23 @@ def split_args(s: str) -> List[str]:
   rv[0] = sysargs.bin
   return rv
 
-# The "oalex" prefix in each of these cases will be replaced
-# with args.bin in split_args().
-errorcases = [
-    ("oalex", "Input filename missing"),
-    ("oalex build --h-out test.h", "Input filename missing"),
-    ("oalex build --h-out test.h test.oalex", "Both .* or neither"),
-    ("oalex build test --h-out test.h", "Input filename missing"),
-    ("oalex build test --h-out test.h test.oalex", "All .* or none"),
-]
+def test_malformed_cmdline_errors():
+  # The "oalex" prefix in each of these cases will be replaced
+  # with args.bin in split_args().
+  errorcases = [
+      ("oalex", "Input filename missing"),
+      ("oalex build --h-out test.h", "Input filename missing"),
+      ("oalex build --h-out test.h test.oalex", "Both .* or neither"),
+      ("oalex build test --h-out test.h", "Input filename missing"),
+      ("oalex build test --h-out test.h test.oalex", "All .* or none"),
+  ]
 
-for cmd, error_re in errorcases:
-  result = subprocess.run(split_args(cmd), capture_output=True)
-  if not re.search(error_re, result.stderr.decode('utf-8')):
-    print(f"Couldn't find the expected error for command: {cmd}")
-    print(f"Expected '{error_re}'\nGot {result.stderr!r}")
-    sys.exit(1)
+  for cmd, error_re in errorcases:
+    result = subprocess.run(split_args(cmd), capture_output=True)
+    if not re.search(error_re, result.stderr.decode('utf-8')):
+      print(f"Couldn't find the expected error for command: {cmd}")
+      print(f"Expected '{error_re}'\nGot {result.stderr!r}")
+      sys.exit(1)
 
 def find_expected_error(filename: str) -> str:
   directive = "# expected-error:"
@@ -46,28 +47,34 @@ def find_expected_error(filename: str) -> str:
         return line[len(directive):].strip()
   return None
 
-testfiles = []
-with os.scandir(sysargs.testdata) as dir_entries:
-  for entry in dir_entries:
-    if entry.name.endswith('.oalex') and entry.is_file():
-      testfiles.append(entry.name)
+def eval_testdata_files():
+  testfiles = []
+  with os.scandir(sysargs.testdata) as dir_entries:
+    for entry in dir_entries:
+      if entry.name.endswith('.oalex') and entry.is_file():
+        testfiles.append(entry.name)
 
-for filename in testfiles:
-  expected_error = find_expected_error(filename)
-  if expected_error:
-    result = subprocess.run(
-               [sysargs.bin, "test", os.path.join(sysargs.testdata, filename)],
-               input="", capture_output=True)
-    assert result.returncode != 0, f"{filename} was expected to cause failure"
-    observed_error = result.stderr.decode('utf-8')
-    assert expected_error in observed_error, \
-           f"Was expecting '{expected_error}' in '{filename}'\n" + \
-           ("Got this instead:\n" + observed_error if observed_error else "")
-  else:
-    result = subprocess.run(
-               [sysargs.bin, "test", os.path.join(sysargs.testdata, filename)],
-               capture_output=True)
-    if result.returncode != 0:
-      print(f"`oalex test` failed on {filename}. Error output:")
-      print(result.stderr.decode("utf-8"))
-      sys.exit(1)
+  for filename in testfiles:
+    expected_error = find_expected_error(filename)
+    if expected_error:
+      result = subprocess.run(
+                 [ sysargs.bin, "test",
+                   os.path.join(sysargs.testdata, filename)
+                 ], input="", capture_output=True)
+      assert result.returncode != 0, f"{filename} was expected to cause failure"
+      observed_error = result.stderr.decode('utf-8')
+      assert expected_error in observed_error, \
+             f"Was expecting '{expected_error}' in '{filename}'\n" + \
+             ("Got this instead:\n" + observed_error if observed_error else "")
+    else:
+      result = subprocess.run(
+                 [ sysargs.bin, "test",
+                   os.path.join(sysargs.testdata, filename)
+                 ], capture_output=True)
+      if result.returncode != 0:
+        print(f"`oalex test` failed on {filename}. Error output:")
+        print(result.stderr.decode("utf-8"))
+        sys.exit(1)
+
+test_malformed_cmdline_errors()
+eval_testdata_files()
