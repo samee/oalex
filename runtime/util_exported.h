ca5a88c36383d17a8ce162f16ecd8f9fadb4abe1
Samee Zahur <szahur@google.com>
2021-07-21 19:36:22 -0700

    Fix move_to_unique

--- runtime/util_exported.h
+++ runtime/util_exported.h
@@ -6,13 +6,8 @@ namespace oalex {
 
 // Enables brace-initialization for variants without naming the type twice.
 template <class T>
-auto move_to_unique(T& t) -> std::unique_ptr<T> {
-  return std::make_unique<T>(std::move(t));
-}
-
-template <class T>
-auto move_to_unique(T&& t) -> std::unique_ptr<T> {
-  return std::make_unique<T>(std::move(t));
+auto move_to_unique(T&& t) -> std::unique_ptr<std::remove_reference_t<T>> {
+  return std::make_unique<std::remove_reference_t<T>>(std::move(t));
 }
 
 template <class V, class ... Args> std::vector<V>
