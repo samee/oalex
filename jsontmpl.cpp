95e61d3d47f19553726ed35dd684f34ee35fa55b
Samee Zahur <szahur@google.com>
2021-07-31 16:28:47 -0700

    Copy-paste jsonloc.* into jsontmpl.*
    A copy-paste followed by an s/JsonLoc/JsonTmpl/. The fmtlib parse
    method has also been removed since we are not copying
    runtime/jsonloc_fmt.h.
    
    The new files are added to CMakeLists, but no other file currently
    includes the new jsontmpl.h header. That will be introduced slowly in
    subsequent commits.

---
+++ jsontmpl.cpp
@@ -0,0 +1,294 @@
+/*  Copyright 2019-2021 The oalex authors.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License. */
+
+#include "jsontmpl.h"
+#include "fmt/format.h"
+#include "runtime/util.h"
+using fmt::format_to;
+using fmt::memory_buffer;
+using oalex::Bug;
+using std::get;
+using std::make_pair;
+using std::pair;
+using std::string;
+using std::string_view;
+
+namespace oalex {
+
+using ErrorValue = JsonTmpl::ErrorValue;
+using Placeholder = JsonTmpl::Placeholder;
+using String = JsonTmpl::String;
+using Map = JsonTmpl::Map;
+using Vector = JsonTmpl::Vector;
+
+[[noreturn]] static void BugUnknownJsonType(const JsonTmpl::Tag& tag) {
+  Bug("Strange JsonTmpl type with enum = {}", int(tag));
+}
+
+void JsonTmpl::copyValue(const JsonTmpl& that) {
+  switch(tag_) {
+    case Tag::ErrorValue: return;
+    case Tag::String:
+      new (&stringValue_) String{that.stringValue_}; return;
+    case Tag::Vector:
+      new (&vectorValue_) Vector(that.vectorValue_); return;
+    case Tag::Map:
+      new (&mapValue_) Map(that.mapValue_); return;
+    case Tag::Placeholder:
+      new (&placeholderValue_) Placeholder{that.placeholderValue_};
+  }
+}
+
+void JsonTmpl::moveValue(JsonTmpl&& that) {
+  switch(tag_) {
+    case Tag::ErrorValue: return;
+    case Tag::String:
+      new (&stringValue_) String{std::move(that.stringValue_)}; return;
+    case Tag::Vector:
+      new (&vectorValue_) Vector(std::move(that.vectorValue_)); return;
+    case Tag::Map:
+      new (&mapValue_) Map(std::move(that.mapValue_)); return;
+    case Tag::Placeholder:
+      new (&placeholderValue_) Placeholder{std::move(that.placeholderValue_)};
+  }
+}
+
+void JsonTmpl::destroyValue() {
+  switch(tag_) {
+    case Tag::ErrorValue: return;
+    case Tag::String: stringValue_.~string(); return;
+    case Tag::Vector: vectorValue_.~vector(); return;
+    case Tag::Map: mapValue_.~vector(); return;
+    case Tag::Placeholder: placeholderValue_.~Placeholder(); return;
+  }
+}
+
+JsonTmpl::JsonTmpl(JsonTmpl&& that)
+  : stPos{that.stPos}, enPos{that.enPos},
+    tag_{that.tag_} {
+  moveValue(std::move(that));
+}
+JsonTmpl::JsonTmpl(const JsonTmpl& that)
+  : stPos{that.stPos}, enPos{that.enPos}, tag_{that.tag_} {
+  copyValue(that);
+}
+JsonTmpl& JsonTmpl::operator=(JsonTmpl&& that) {
+  destroyValue();
+  this->stPos = that.stPos;
+  this->enPos = that.enPos;
+  this->tag_ = that.tag_;
+  moveValue(std::move(that));
+  return *this;
+}
+JsonTmpl& JsonTmpl::operator=(const JsonTmpl& that) {
+  destroyValue();
+  this->stPos = that.stPos;
+  this->enPos = that.enPos;
+  this->tag_ = that.tag_;
+  copyValue(that);
+  return *this;
+}
+JsonTmpl::~JsonTmpl() { destroyValue(); }
+
+string_view JsonTmpl::tagName() const {
+  switch(tag_) {
+    case Tag::ErrorValue: return "ErrorValue";
+    case Tag::String: return "String";
+    case Tag::Vector: return "Vector";
+    case Tag::Map: return "Map";
+    case Tag::Placeholder: return "Placeholder";
+    default: BugUnknownJsonType(tag_);
+  }
+}
+
+ssize_t JsonTmpl::mapLinearFind(const Map& m, std::string_view k) {
+  for(ssize_t i=0; i<ssize(m); ++i) if(k == m[i].first) return i;
+  return -1;
+}
+
+void JsonTmpl::mapSort(Map& m) {
+  auto byfirst = +[](const Map::value_type& a, const Map::value_type& b)
+    { return a.first < b.first; };
+  sort(m.begin(), m.end(), byfirst);
+
+  for(size_t i=1; i<m.size(); ++i) if(m[i-1].first == m[i].first)
+    Bug("maps are supposed to be key-disjoint");
+}
+
+// Template parameters parameterize over const-qualifiers.
+template <class PlaceholderMap, class JsonTmplInput>
+static void allPlaceholdersImpl(PlaceholderMap& rv, JsonTmplInput& json) {
+  if(auto* p = json.getIfPlaceholder()) rv.emplace_back(p->key, &json);
+  else if(json.holdsString()) return;
+  else if(auto* v = json.getIfVector())
+    for(auto& elt : *v) allPlaceholdersImpl(rv,elt);
+  else if(auto* m = json.getIfMap())
+    for(auto& [k,v] : *m) allPlaceholdersImpl(rv,v);
+  else BugUnknownJsonType(json.tag());
+}
+
+auto JsonTmpl::allPlaceholders() -> PlaceholderMap {
+  auto byfirst = [](auto a, auto b) { return a.first < b.first; };
+  PlaceholderMap rv;
+  allPlaceholdersImpl(rv,*this);
+  std::sort(rv.begin(), rv.end(), byfirst);
+  return {rv.begin(), rv.end()};
+}
+
+auto JsonTmpl::allPlaceholders() const -> ConstPlaceholderMap {
+  auto byfirst = [](auto a, auto b) { return a.first < b.first; };
+  ConstPlaceholderMap rv;
+  allPlaceholdersImpl(rv,*this);
+  std::sort(rv.begin(), rv.end(), byfirst);
+  return {rv.begin(), rv.end()};
+}
+
+size_t JsonTmpl::substitute(const PlaceholderMap& pmap, string_view key,
+                           const JsonTmpl& json) {
+  auto locmp = +[](const PlaceholderMap::value_type& kv, string_view key)
+    { return kv.first < key; };
+  auto hicmp = +[](string_view key, const  PlaceholderMap::value_type& kv)
+    { return key < kv.first; };
+  auto lo = std::lower_bound(pmap.begin(), pmap.end(), key, locmp);
+  auto hi = std::upper_bound(pmap.begin(), pmap.end(), key, hicmp);
+  size_t rv=0;
+  for(auto it=lo; it!=hi; ++it) {
+    *it->second = json;
+    ++rv;
+  }
+  return rv;
+}
+
+// assumes (stPos == npos) == (enPos == npos)
+static bool childIsGood(const JsonTmpl& parent, const JsonTmpl& child) {
+  if(!child.substitutionsOk()) return false;
+  else if(child.stPos == JsonTmpl::npos) return true;
+  else if(parent.stPos == JsonTmpl::npos) return false;
+  else if(child.stPos < parent.stPos) return false;
+  else if(child.enPos > parent.enPos) return false;
+  else return true;
+}
+
+bool JsonTmpl::substitutionsOk() const {
+  if(holdsPlaceholder()) return false;
+  if((this->stPos==JsonTmpl::npos) != (this->enPos==JsonTmpl::npos))
+    return false;
+
+  if(auto* v = getIfVector()) {
+    for(auto& elt : *v) if(!childIsGood(*this,elt)) return false;
+  } else if(auto* m = getIfMap()) {
+    for(auto& [k,v] : *m) if(!childIsGood(*this,v)) return false;
+  } else if(!holdsString()) {
+    BugUnknownJsonType(tag_);
+  }
+  return true;
+}
+
+static void printString(fmt::memory_buffer& buf, string_view s) {
+  format_to(buf, "\"");
+  // If this changes, please change lexQuotedEscape as well.
+  // TODO write test.
+  for(char ch : s) {
+    if(ch=='"') format_to(buf, "\"");
+    else if(ch=='\\') format_to(buf, "\\\\");
+    else if(ch=='\n') format_to(buf, "\\n");
+    else if(ch=='\t') format_to(buf, "\\t");
+    else if(isprint(ch))
+      format_to(buf, "{}", ch);  // check this after '"' and '\\'.
+    else format_to(buf, "\\x{:02x}", ch);
+  }
+  format_to(buf, "\"");
+}
+
+static string_view assertIdent(string_view ctx, string_view s) {
+  if(s.empty()) Bug("{}: Identifier can't be null.", ctx);
+  if(isdigit(s[0])) Bug("{}: Identifier can't start with a digit.", ctx);
+  for(size_t i=0; i<s.size(); ++i) if(s[i]!='_' && !isalnum(s[i]))
+    Bug("{}: Invalid identifier character at position {}.", ctx, i);
+  return s;
+}
+
+static void prettyPrint(fmt::memory_buffer& buf,
+                        size_t indent, const JsonTmpl& json,
+                        bool quoteMapKeys) {
+  if(auto* p = json.getIfPlaceholder())
+    format_to(buf, "{}", assertIdent(__func__, p->key));
+  else if(auto* s = json.getIfString()) printString(buf, *s);
+  else if(auto* v = json.getIfVector()) {
+    format_to(buf, "[\n");
+    bool first = true;
+    for(const JsonTmpl& elt : *v) {
+      if(!first) format_to(buf, ",\n");
+      first = false;
+      format_to(buf, "{:{}}", "", indent+2);
+      prettyPrint(buf, indent+2, elt, quoteMapKeys);
+    }
+    format_to(buf, "\n{:{}}]", "", indent);
+  }else if(auto* m = json.getIfMap()) {
+    format_to(buf, "{{\n");
+    bool first = true;
+    for(auto& [k,v] : *m) {
+      if(!first) format_to(buf, ",\n");
+      first = false;
+      if(quoteMapKeys) {
+        format_to(buf, "{:{}}\"{}\": ", "", indent+2, assertIdent(__func__,k));
+      }else {
+        format_to(buf, "{:{}}{}: ", "", indent+2, assertIdent(__func__,k));
+      }
+      prettyPrint(buf, indent+2, v, quoteMapKeys);
+    }
+    format_to(buf, "\n{:{}}}}", "", indent);
+  }else if(json.holdsErrorValue()) {
+    format_to(buf, "(error_value)");
+  }else BugUnknownJsonType(json.tag());
+}
+
+string JsonTmpl::prettyPrint(size_t indent) const {
+  fmt::memory_buffer buf;
+  oalex::prettyPrint(buf, indent, *this, false);
+  return fmt::to_string(buf);
+}
+
+string JsonTmpl::prettyPrintJson(size_t indent) const {
+  fmt::memory_buffer buf;
+  oalex::prettyPrint(buf, indent, *this, true);
+  return fmt::to_string(buf);
+}
+
+bool JsonTmpl::supportsEquality() const {
+  if(holdsPlaceholder() || holdsErrorValue()) return false;
+  if(holdsString()) return true;
+  if(auto* vec = getIfVector()) {
+    for(auto& v : *vec) if(!v.supportsEquality()) return false;
+    return true;
+  }
+  if(auto* m = getIfMap()) {
+    for(auto& [k,v] : *m) if(!v.supportsEquality()) return false;
+    return true;
+  }
+  BugUnknownJsonType(tag_);
+}
+
+bool JsonTmpl::operator==(const JsonTmpl& that) const {
+  if(this->tag_ != that.tag_) return false;
+
+  if(holdsPlaceholder() || holdsErrorValue())
+    Bug("supportsEquality() is false");
+  if(auto* s = getIfString()) return *s == *that.getIfString();
+  if(auto* v = getIfVector()) return *v == *that.getIfVector();
+  if(auto* m = getIfMap()) return *m == *that.getIfMap();
+  BugUnknownJsonType(tag_);
+}
+
+}  // namespace oalex
---
+++ jsontmpl.h
@@ -0,0 +1,162 @@
+/*  Copyright 2019-2021 The oalex authors.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License. */
+
+#pragma once
+
+#include <limits>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace oalex {
+
+// Even though we call it json, we don't in fact support numbers, booleans,
+// or null. We do support an error type. Together with strings, those two are
+// the only atomic types.
+// stPos and enPos can remain JsonTmpl::npos, if they only have hardcoded values.
+// The other difference is that we treat strings as byte-strings, not utf-8.
+// This also causes a difference in our backslash escaping conventions.
+// prettyPrint() also returns something closer to protobufs than json.
+// In other words, this is a complete abuse of the term "json".
+//
+// Another design wart added later: originally the stPos and enPos fields were
+// meant to indicate locations in user input, not in oalex source. But an
+// exception was later made for JsonTmpl::Placeholder, since it never shows up
+// in outputs parsed out of user input.
+//
+// At some point in the future, we might separate out these into two types:
+//
+//   * The output produced by parsing user input.
+//   * The braced portion of an 'outputs' stanza in a rule or example.
+//     This will include ellipsis as a variant, but not error values.
+class JsonTmpl {
+ public:
+  enum class Tag { ErrorValue, String, Vector, Map, Placeholder };
+  static constexpr size_t npos = std::numeric_limits<size_t>::max();
+  struct ErrorValue {};
+  using String = std::string;
+  using Vector = std::vector<JsonTmpl>;
+  using Map = std::vector<std::pair<std::string, JsonTmpl>>;  // sorted keys.
+  struct Placeholder { std::string key; };
+
+  size_t stPos=npos, enPos=npos;
+  static ssize_t mapLinearFind(const Map& m, std::string_view k);
+  static void mapSort(Map& m);
+
+  // conversion constructors.
+  JsonTmpl() = delete;
+  JsonTmpl(ErrorValue) : tag_{Tag::ErrorValue}, errorValue_{} {}
+  JsonTmpl(Placeholder p, size_t st, size_t en) : stPos{st}, enPos{en},
+    tag_{Tag::Placeholder}, placeholderValue_{std::move(p)} {}
+  JsonTmpl(String s) : tag_{Tag::String}, stringValue_{std::move(s)} {}
+  JsonTmpl(Vector v) : tag_{Tag::Vector}, vectorValue_(std::move(v)) {}
+  JsonTmpl(Map m) : tag_{Tag::Map}, mapValue_(std::move(m)) {}
+
+  JsonTmpl(JsonTmpl&& that);
+  JsonTmpl(const JsonTmpl& that);
+  JsonTmpl& operator=(JsonTmpl&& that);
+  JsonTmpl& operator=(const JsonTmpl& that);
+  ~JsonTmpl();
+
+  // Or rename type to Tag::Error.
+  bool holdsErrorValue() const { return tag_ == Tag::ErrorValue; }
+  bool holdsString() const { return tag_ == Tag::String; }
+  bool holdsVector() const { return tag_ == Tag::Vector; }
+  bool holdsMap() const { return tag_ == Tag::Map; }
+  bool holdsPlaceholder() const { return tag_ == Tag::Placeholder; }
+
+  String* getIfString() { return holdsString() ? &stringValue_ : nullptr; }
+  const String* getIfString() const
+    { return holdsString() ? &stringValue_ : nullptr; }
+  Placeholder* getIfPlaceholder()
+    { return holdsPlaceholder() ? &placeholderValue_ : nullptr; }
+  const Placeholder* getIfPlaceholder() const
+    { return holdsPlaceholder() ? &placeholderValue_ : nullptr; }
+  Vector* getIfVector() { return holdsVector() ? &vectorValue_ : nullptr; }
+  const Vector* getIfVector() const
+    { return holdsVector() ? &vectorValue_ : nullptr; }
+  Map* getIfMap() { return holdsMap() ? &mapValue_ : nullptr; }
+  const Map* getIfMap() const { return holdsMap() ? &mapValue_ : nullptr; }
+
+  Tag tag() const { return tag_; }
+  std::string_view tagName() const;
+
+  // Note that allPlaceholders() is a non-const member method, since it returns
+  // non-const JsonTmpl pointers to various internal components. Pretty much any
+  // direct mutation to *this will invalidate these pointers: only
+  // substitute() is safe.
+  using PlaceholderMap = std::vector<std::pair<std::string, JsonTmpl*>>;
+  PlaceholderMap allPlaceholders();
+
+  // The Const version is used for pre-substitution processing, by functions
+  // that only accept const JsonTmpl objects but extract the identifier list.
+  using ConstPlaceholderMap
+    = std::vector<std::pair<std::string, const JsonTmpl*>>;
+  ConstPlaceholderMap allPlaceholders() const;
+
+  // Returns number of substitutions made. Zero if key doesn't exist.
+  size_t substitute(const PlaceholderMap& pmap, std::string_view key,
+                    const JsonTmpl& json);
+
+  // Check if all placeholders have been substituted.
+  // Check if child intervals are covered by parent intervals (ignoring npos).
+  // Check if child has a valid interval, so does parent.
+  // Check if all (stPos==npos) == (enPos==npos).
+  // Typically, this should be checked after all substitutions are made.
+  bool substitutionsOk() const;
+
+  // The first line is never indented. No newline follows the last character.
+  // Corollary: String and Placeholders are never indented,
+  //   and are not newline-terminated.
+  std::string prettyPrint(size_t indent=0) const;
+  std::string prettyPrintJson(size_t indent=0) const;
+  std::string prettyPrintWithLocs(size_t indent=0) const;  // TODO
+
+  // This is false iff we have any ErrorValue or Placeholder nodes.
+  //
+  // Dev-notes: we may later replace this with hasErrorValue().
+  //   See notes on operator==() for reasons.
+  bool supportsEquality() const;
+
+  // Dev-notes: Right now this equality check is only used for `oalex test`.
+  //   If those tests later acquire support for placeholders or omitted fields,
+  //   we will have to generalize this to some relaxed notion of matching.
+  bool operator==(const JsonTmpl& that) const;
+  bool operator!=(const JsonTmpl& that) const { return !(*this == that); }
+ private:
+  Tag tag_;
+  union {
+    ErrorValue errorValue_;
+    String stringValue_;
+    Vector vectorValue_;
+    Map mapValue_;
+    Placeholder placeholderValue_;
+  };
+  void destroyValue();
+  void copyValue(const JsonTmpl& that);
+  void moveValue(JsonTmpl&& that);
+};
+
+inline bool isPlaceholder(const JsonTmpl& jstmpl, std::string_view pname) {
+  if(auto* p = jstmpl.getIfPlaceholder()) return p->key == pname;
+  else return false;
+}
+
+inline JsonTmpl moveEltOrEmpty(JsonTmpl::Map& m, std::string_view key) {
+  for(ssize_t i=0; i<(ssize_t)m.size(); ++i)
+    if(m[i].first == key) return std::move(m[i].second);
+  return JsonTmpl::Map{};
+}
+
+}  // namespace oalex
