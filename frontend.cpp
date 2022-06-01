ae4c96e89a2ac44d3e92e3d1fc61bd7970acb854
Samee Zahur <szahur@google.com>
2022-05-31 19:24:54 -0700

    Support `(lhs ~ rhs)` in single-line rules

--- frontend.cpp
+++ frontend.cpp
@@ -757,8 +757,13 @@ makeRuleExprAtom(DiagsDest ctx, const ExprToken& tok) {
       else
         Error(ctx, *gs, "Strings need to be 'quoted'");
     }else return make_unique<RuleExprSquoted>(string(*gs));
+  }else if(auto* seg = get_if<WholeSegment>(&tok)) {
+    if(resemblesIdent(*seg)) {
+      Ident id = requireIdent(tok, ctx);
+      if(id) return make_unique<RuleExprIdent>(std::move(id));
+    }else Error(ctx, *seg, "Not a valid rule definition");
   }else if(!holds_alternative<BracketGroup>(tok))
-    Error(ctx, tok, "Not a valid rule definition");
+    Bug("{}: Unexpected token type {}", __func__, exprTagName(tok));
   return nullptr;
 }
 
@@ -817,6 +822,13 @@ parseSingleLineRule(const Ident& ruleName, vector<ExprToken> linetoks,
   requireEol(linetoks, 4, ctx);
   unique_ptr<const RuleExpr> rxpr = makeRuleExpr(linetoks[3], ctx);
   if(!rxpr) return;
+  if(auto* seg = get_if<WholeSegment>(&linetoks[3])) {
+    // TODO: Fix RuleExprIdent codegen to alias and not nest single idents.
+    if(resemblesIdent(*seg))
+      Error(ctx, *seg,
+            format("Aliasing unimplemented. Use `({})` instead", **seg) );
+    return;
+  }
   ssize_t newIndex = rl.defineIdent(ctx, ruleName);
   if(newIndex != -1) assignRuleExpr(ctx, *rxpr, rl, newIndex);
 }
